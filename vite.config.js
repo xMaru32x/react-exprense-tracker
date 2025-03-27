7e2a0b4e23cf99b855010572aeda86c20c87aa22
xmaru32x <ma.amaya.332@gmail.com>
2025-03-26 21:52:38 -0700

    GitHub page adaptation

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/react-expense-tracker",
   plugins: [
     react(),
     tailwindcss(),
